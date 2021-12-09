60c0b34e3d71cae34d7127bcb9d0ae590ce70475
Tkeshick <dianatkesh@gmail.com>
2021-12-09 18:13:59 +0300

    сделан логин логаут (пользовательский)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index');
+router.get('/', (req, res) => {
+  res.render('index', {
+    user: req.session.user,
+  });
 });
 
+// router.get('/', (req, res) => {
+//   if (req.session.user.isSession) {
+//     res.render('index', {
+//       isSession: req.session.user.isSession,
+//     });
+//   } else {
+//     res.render('index');
+//   }
+// });
 module.exports = router;
--- routes/welcome.js
+++ routes/welcome.js
@@ -1,9 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('welcome');
+
+router.get('/', (req, res) => {
+  res.render('welcome', {
+    user: req.session.user,
+  });
 });
 
 module.exports = router;
